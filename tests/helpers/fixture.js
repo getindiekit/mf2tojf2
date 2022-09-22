ff9abeb8f048e1cd353619a739ffac88c9df37de
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-09-22 14:15:28 +0100

    style: lint javascript

--- tests/helpers/fixture.js
+++ tests/helpers/fixture.js
@@ -7,7 +7,7 @@ import { __dirname } from "../../index.js";
  * @param {string} encoding String encoding
  * @returns {Promise|object} File contents
  */
-export const getFixture = (filename, encoding = "utf-8") => {
+export const getFixture = (filename, encoding = "utf8") => {
   const file = path.resolve(__dirname, `tests/fixtures/${filename}`);
   return fs.readFileSync(file, { encoding });
 };
